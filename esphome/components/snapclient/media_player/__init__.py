d667bb77bbb444fab5ef12ba2083c4a92040a06e
raul <raul11@freenet.de>
2026-03-04 22:10:32 +0100

    Fix build

--- esphome/components/snapclient/media_player/__init__.py
+++ esphome/components/snapclient/media_player/__init__.py
@@ -22,7 +22,7 @@ AUTO_LOAD = ["mdns", "socket"]
 CONF_HOSTNAME = "hostname"
 CONF_MUTE_PIN = "mute_pin"
 
-SNAPCLIENT_GIT_VERSION = "f3ea882760dbbd0509356f6b9eeb54f87c97452d"
+SNAPCLIENT_GIT_VERSION = "1dfe76aa3b057f60985ebec5a867b073924eeef1"
 SNAPCLIENT_GIT_REPO = "https://github.com/luar123/snapclient.git"
 
 snapclient_ns = cg.esphome_ns.namespace("snapclient")
